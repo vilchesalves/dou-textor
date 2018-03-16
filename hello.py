9d92ee46d2ab543f00df99e2614fd6f64ab677e0
Gabriel Vilches Alves <vilchesalves@gmail.com>
2018-03-16 15:54:17 +1300

    secondary commit

---
+++ hello.py
@@ -0,0 +1,9 @@
+import re
+from util import load_random_substring, load_file
+
+random_file_content = load_random_substring(load_file(), 5000)
+
+lines = random_file_content.split('\n')[1:-1]
+words = re.compile(r'\s').split(random_file_content)
+
+print([w for w in words])
---
+++ util.py
@@ -0,0 +1,26 @@
+import textract
+import os.path
+from random import randint
+
+def load_file ():
+    script_folder = os.path.dirname(os.path.realpath(__file__))
+
+    file_name = 'DOU1_20180315'
+    txt_path = '{}/txt/{}.txt'.format(script_folder, file_name)
+
+    if os.path.exists(txt_path):
+        fh = open(txt_path)
+    else:
+        extracted_text = textract.process('./dou/{}.pdf'.format(file_name))
+        fh = open(txt_path, 'w')
+        fh.write(extracted_text)
+    
+    file_content = fh.read()
+    
+    return file_content
+
+def load_random_substring (string, length):
+    start = randint(0, len(string) - length)
+    end = start + length
+
+    return string[start:end]
