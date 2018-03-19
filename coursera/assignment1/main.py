188dcd9f7fbab90557821b6afa66e5f8ca2c1edc
Gabriel Alves Vilches <vilchesalves@gmail.com>
2018-03-19 22:23:08 +1300

    19/3/2018

--- coursera/assignment1/main.py
+++ coursera/assignment1/main.py
@@ -14,22 +14,26 @@ regs = [
     # re.compile('([0-9]{2})\/([0-9]{2})\/([0-9]{4})'),
 ]
 
-def match_to_regs (line, regs):
-    for reg in regs:
-        match = reg.match(line)
-        if match:
-            return match
-    
-    return line
+def match_to_regs (agg, curr):
+    if type(agg) is not dict:
+        agg = {
+            'lines': [],
+            'parsed': [],
+            'unparsed': [],
+        }
+    agg['lines'].append(curr)
+    return agg
 
+# print(2, lines[:3])
 
-lines = map(lambda line: match_to_regs(line, regs), lines)
+# lines = map(lambda line: match_to_regs(line, regs), lines)
+lines = reduce(match_to_regs, lines[:5])
 # df = pd.Series(doc)
 
 print(lines)
 
-print({
-    'lines': len(lines),
-    'parsed': len(parsed),
-    'unparsed': len(unparsed),
-})
+# print({
+#     'lines': len(lines),
+#     'parsed': len(parsed),
+#     'unparsed': len(unparsed),
+# })
