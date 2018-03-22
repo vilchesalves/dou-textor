c55a9e34c6a66f75d374021e2a89a6d475ad4f26
Gabriel Vilches Alves <vilchesalves@gmail.com>
2018-03-22 14:19:37 +1300

    fill empty days, months and add month indexes

--- coursera/assignment1/main.py
+++ coursera/assignment1/main.py
@@ -1,3 +1,5 @@
+from calendar import month_abbr
+from functools import reduce
 import pandas as pd
 import re
 
@@ -10,31 +12,66 @@ with open('../dates.txt') as file:
         lines.append(line)
 
 regs = [
-    re.compile(r'(?P<month>[0-9]{1,2})[/-](?P<day>[0-9]{1,2})?[/-]?(?P<year>(19|20)?[0-9]{2}?)'),
+    re.compile(r'(?P<month>[0-9]{1,2})/?(?P<day>[0-9]{1,2})?/(?P<year>(19|20)?[0-9]{2}?)[^/]'),
+    re.compile(r'(?P<month>[0-9]{1,2})-(?P<day>[0-9]{1,2})?-(?P<year>(19|20)?[0-9]{2}?)'),
     re.compile(r'(?P<day>[0-9]{2}) (?P<month>[a-zA-z]{3,8}) (?P<year>[0-9]{4})'),
     re.compile(r'(?P<month>(.(an[au]{2}ry|ebruary|arch|pril|ay|une|uly|ugust|eptember|ctober|ovember|eceme?ber)|(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)))\.? ?(?P<day>[0-9]{1,2})?,? ?(?P<year>[0-9]{4})'),
     re.compile(r'(?P<year>[0-9]{4})'),
 ]
 
+enumerate_months = {v: k for k,v in enumerate(month_abbr)}
+
 def match_to_regs (agg, curr):
-    if type(agg) is not dict:
-        agg = match_to_regs({
-            'parsed': [],
-            'unparsed': [],
-        }, agg)
-    
+    global enumerate_months
+
     for reg in regs:
         match = reg.search(curr)
         
         if match:
-            agg['parsed'].append((match.groupdict(), curr))
+            parsed = match.groupdict()
+            
+            # fill in empty indexes
+            
+            if (
+                'day' not in match.groupdict()
+                or parsed['day'] == None
+            ):
+                parsed['day'] = '1'
+            
+            if 'month' not in match.groupdict():
+                parsed['month'] = '1'
+
+            # transform months into indexes
+            
+            if (
+                isinstance(parsed['month'], str)
+                and len(parsed['month']) > 2
+            ):
+                month_index = enumerate_months[parsed['month'][:3]]
+                parsed['month'] = str(month_index)
+            
+            # fill in incomplete numbers
+            
+            if len(parsed['day']) == 1:
+                parsed['day'] = '0' + parsed['day']
+            
+            if len(parsed['month']) == 1:
+                parsed['month'] = '0' + parsed['month']
+            
+            if len(parsed['year']) == 2:
+                parsed['year'] = '19' + parsed['year']
+                
+            agg['parsed'].append((parsed, curr))
             break
     else:
         agg['unparsed'].append(curr)
 
     return agg
 
-lines = reduce(match_to_regs, lines)
+lines = reduce(match_to_regs, lines, {
+            'parsed': [],
+            'unparsed': [],
+        })
 # df = pd.Series(doc)
 
 print({
@@ -42,5 +79,5 @@ print({
     'unparsed': len(lines['unparsed']),
 })
 
-for date in lines['parsed']:
-    print(date)
+for i, date in enumerate(lines['parsed']):
+    print(i, date)
